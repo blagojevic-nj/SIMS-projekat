5816b170ae7c807751b6c2023b8e2f76f8bb86ed
albertmakan <68916222+albertmakan@users.noreply.github.com>
2020-08-20 20:30:45 +0200

    Uradjen je PretragaPanel

--- Projekat/src/view/PretragaPanel.java
+++ Projekat/src/view/PretragaPanel.java
@@ -2,43 +2,202 @@ package view;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
+import manageri.KategorijaManager;
 import manageri.ProizvodManager;
 import manageri.ReceptManager;
 
 public class PretragaPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
+	private JComboBox<String> sastojciBox, nepozeljniBox, kategorijeBox;
+	private JButton dodaj1, dodaj2, dodaj3, btnPretrazi, btnOcisti;
+	private JList<String> sastojciList, nepozeljniList, kategorijeList;
+	private JRadioButton rbtnSvi;
 	
 	private ReceptManager rm = ReceptManager.getInstance();
 	private ProizvodManager pm = ProizvodManager.getInstance();
+	private KategorijaManager km = KategorijaManager.getInstance();
 	
 	public PretragaPanel() {
 		setBackground(Color.LIGHT_GRAY);
 		setBounds(0, 0, 300, 700);
 		setLayout(null);
 		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(50, 100, 200, 5);
-		add(separator);
-		
-		
 		JLabel lblPretraga = new JLabel("Pretraga");
 		lblPretraga.setFont(new Font("Tahoma", Font.PLAIN, 35));
-		lblPretraga.setBounds(50, 30, 200, 60);
+		lblPretraga.setBounds(50, 30, 200, 50);
 		add(lblPretraga);
 		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(50, 80, 200, 5);
+		add(separator);
 		
-		DefaultComboBoxModel<String> dml = new DefaultComboBoxModel<String>();
-		for (String s : pm.getNaziviProizvoda()) dml.addElement(s);
-		JComboBox<String> sastojciBox = new JComboBox<String>(dml);
-		sastojciBox.setBounds(50, 140, 200, 25);
+		JLabel lblSastojci = new JLabel("Sastojci:");
+		lblSastojci.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblSastojci.setBounds(30, 100, 200, 25);
+		add(lblSastojci);
+		
+		DefaultComboBoxModel<String> proizvodBoxModel = new DefaultComboBoxModel<String>();
+		proizvodBoxModel.addElement("");
+		for (String s : pm.getNaziviProizvoda()) proizvodBoxModel.addElement(s);
+		
+		sastojciBox = new JComboBox<String>(proizvodBoxModel);
+		sastojciBox.setBounds(30, 120, 200, 25);
 		add(sastojciBox);
+		
+		dodaj1 = new JButton(new ImageIcon("data/ikonice/add.png"));
+		dodaj1.setBounds(240, 120, 25, 25);
+		add(dodaj1);
+		
+		DefaultListModel<String> sastojciListModel = new DefaultListModel<String>();
+		sastojciList = new JList<String>(sastojciListModel);
+		JScrollPane sastojciSP = new JScrollPane(sastojciList);
+		sastojciSP.setBounds(30, 150, 200, 75);
+		add(sastojciSP);
+		
+		rbtnSvi = new JRadioButton("Svi navedeni sastojci su u receptu");
+		rbtnSvi.setBounds(30, 230, 230, 25);
+		add(rbtnSvi);
+		
+		JSeparator separator1 = new JSeparator();
+		separator1.setBounds(30, 270, 230, 5);
+		add(separator1);
+		
+		JLabel lblNepozeljni = new JLabel("Nepozeljni sastojci:");
+		lblNepozeljni.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNepozeljni.setBounds(30, 280, 200, 25);
+		add(lblNepozeljni);
+		
+		nepozeljniBox = new JComboBox<String>(proizvodBoxModel);
+		nepozeljniBox.setBounds(30, 300, 200, 25);
+		add(nepozeljniBox);
+		
+		dodaj2 = new JButton(new ImageIcon("data/ikonice/add.png"));
+		dodaj2.setBounds(240, 300, 25, 25);
+		add(dodaj2);
+		
+		DefaultListModel<String> nepozeljniListModel = new DefaultListModel<String>();
+		nepozeljniList = new JList<String>(nepozeljniListModel);
+		JScrollPane nepozeljniSP = new JScrollPane(nepozeljniList);
+		nepozeljniSP.setBounds(30, 330, 200, 50);
+		add(nepozeljniSP);
+		
+		JSeparator separator2 = new JSeparator();
+		separator2.setBounds(30, 390, 230, 5);
+		add(separator2);
+		
+		JLabel lblKategorije = new JLabel("Kategorije:");
+		lblKategorije.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblKategorije.setBounds(30, 400, 200, 25);
+		add(lblKategorije);
+		
+		DefaultComboBoxModel<String> kategorijaBoxModel = new DefaultComboBoxModel<String>();
+		kategorijaBoxModel.addElement("");
+		for (String k : km.getNaziviKategorija()) kategorijaBoxModel.addElement(k);
+		
+		kategorijeBox = new JComboBox<String>(kategorijaBoxModel);
+		kategorijeBox.setBounds(30, 420, 200, 25);
+		add(kategorijeBox);
+		
+		dodaj3 = new JButton(new ImageIcon("data/ikonice/add.png"));
+		dodaj3.setBounds(240, 420, 25, 25);
+		add(dodaj3);
+		
+		DefaultListModel<String> kategorijeListModel = new DefaultListModel<String>();
+		kategorijeList = new JList<String>(kategorijeListModel);
+		JScrollPane kategorijeSP = new JScrollPane(kategorijeList);
+		kategorijeSP.setBounds(30, 450, 200, 50);
+		add(kategorijeSP);
+		
+		JSeparator separator3 = new JSeparator();
+		separator3.setBounds(30, 510, 230, 5);
+		add(separator3);
+		
+		JLabel lblVreme = new JLabel("Vreme pripreme maks.:");
+		lblVreme.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblVreme.setBounds(30, 520, 200, 25);
+		add(lblVreme);
+		
+		SpinnerNumberModel sModel = new SpinnerNumberModel(10, 10, 300, 5);
+		JSpinner spinner = new JSpinner(sModel);
+		//spinner.setEditor(new JSpinner.DefaultEditor(spinner));
+		spinner.setBounds(190, 520, 40, 25);
+		add(spinner);
+
+		btnPretrazi = new JButton("Pretrazi", new ImageIcon("data/ikonice/search.png"));
+		btnPretrazi.setBounds(170, 660, 120, 30);
+		add(btnPretrazi);
+		
+		btnOcisti = new JButton("Ocisti");
+		btnOcisti.setBounds(20, 660, 120, 30);
+		add(btnOcisti);
+		
+		dodaj1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (sastojciBox.getSelectedIndex() != 0) {
+					sastojciListModel.addElement((String) sastojciBox.getSelectedItem());
+				}
+			}
+		});
+		
+		dodaj2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (nepozeljniBox.getSelectedIndex() != 0) {
+					nepozeljniListModel.addElement((String) nepozeljniBox.getSelectedItem());
+				}
+			}
+		});
+		
+		dodaj3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (kategorijeBox.getSelectedIndex() != 0) {
+					kategorijeListModel.addElement((String) kategorijeBox.getSelectedItem());
+				}
+			}
+		});
+		
+		btnPretrazi.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				preuzmiArgumenteIPretrazi();
+			}
+		});
+		
+		btnOcisti.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sastojciBox.setSelectedIndex(0);
+				nepozeljniBox.setSelectedIndex(0);
+				kategorijeBox.setSelectedIndex(0);
+				sastojciListModel.removeAllElements();
+				nepozeljniListModel.removeAllElements();
+				kategorijeListModel.removeAllElements();
+				rbtnSvi.setSelected(false);
+				spinner.setValue(10);
+			}
+		});
+	}
+	
+	private void preuzmiArgumenteIPretrazi() {
 		
 	}
 
