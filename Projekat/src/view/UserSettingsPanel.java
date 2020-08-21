2aeedc3e3f5a1d51b6ea4bd5982b19ad46fcad7f
Lule99 <lkureljusic@ymail.com>
2020-08-21 12:30:06 +0200

    UserPanel

--- Projekat/src/view/UserSettingsPanel.java
+++ Projekat/src/view/UserSettingsPanel.java
@@ -5,15 +5,31 @@ import javax.swing.JPanel;
 import manageri.KorisnikManager;
 import model.Nalog;
 import javax.swing.JSplitPane;
+import javax.swing.UIManager;
+
 import java.awt.Choice;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.Font;
 
 public class UserSettingsPanel extends JPanel {
 	
+<<<<<<< Updated upstream
 	private static final long serialVersionUID = -1716355102957653930L;
+=======
+	JPanel main;
+>>>>>>> Stashed changes
 	KorisnikManager km;
 	Nalog trenutni;
 	
@@ -21,11 +37,43 @@ public class UserSettingsPanel extends JPanel {
 		km = manager;
 		trenutni = trenutniNalog;
 		setLayout(null);
-		setBounds(60, 30, 1040, 650);
+		setBounds(500, 30, 1040, 650);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 10, 285, 630);
-		add(panel);
+		main = new JPanel();
+		main.setLayout(null);
+		main.setBounds(10, 10, 285, 630);
+		add(main);
+		setBackground(Color.white);
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(0, 472, 285, 60);
+		lblNewLabel.setForeground(Color.black);
+		main.add(lblNewLabel);
+		
+		
+		lblNewLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+		    public void mouseEntered(java.awt.event.MouseEvent evt) {
+		    	lblNewLabel.setBackground(Color.white);
+		    	lblNewLabel.setOpaque(true);
+		    }
 
+		    public void mouseExited(java.awt.event.MouseEvent evt) {
+		    	lblNewLabel.setBackground(UIManager.getColor("control"));
+		    }
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
